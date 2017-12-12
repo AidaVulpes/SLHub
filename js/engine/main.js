3cb493e059a15603de69ea68f552ab9edda8be65
GarfID <garfid.n.rena@gmail.com>
2017-12-12 12:06:31 +0300

    Loader upd 2

--- js/engine/main.js
+++ js/engine/main.js
@@ -1,10 +1,10 @@
 $( function() {
     if(($(document).height() - $('.nav').height()) < $(document).width()){
-        var loaderH = $(document).height() - $('.nav').height();
-        var loaderW= $(document).height() - $('.nav').height();
-    } else {
         var loaderH = $(document).width();
         var loaderW= $(document).width();
+    } else {
+        var loaderH = $(document).height() - $('.nav').height();
+        var loaderW= $(document).height() - $('.nav').height();
     }
     $(".loader").height(loaderH);
     $(".loader").width(loaderW);
