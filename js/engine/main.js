07d769c874ca860aab6f193953e53cafcf7e3917
GarfID <garfid.n.rena@gmail.com>
2017-12-12 12:05:38 +0300

    Loader upd 1

--- js/engine/main.js
+++ js/engine/main.js
@@ -1,3 +1,12 @@
 $( function() {
+    if(($(document).height() - $('.nav').height()) < $(document).width()){
+        var loaderH = $(document).height() - $('.nav').height();
+        var loaderW= $(document).height() - $('.nav').height();
+    } else {
+        var loaderH = $(document).width();
+        var loaderW= $(document).width();
+    }
+    $(".loader").height(loaderH);
+    $(".loader").width(loaderW);
     $( ".wrap" ).tabs();
 } );
